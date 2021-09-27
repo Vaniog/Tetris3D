1316d071fe7931cadff5e9154d1975389912c2a4
Vaniog <forvantar@gmail.com>
2021-09-27 22:42:17 +0300

    some files for git check

--- app/src/main/java/com/example/tetris3d/MainActivity.kt
+++ app/src/main/java/com/example/tetris3d/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.tetris3d
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener{
+            startActivity(Intent(this, GameActivity::class.java))
+        }
     }
 }
